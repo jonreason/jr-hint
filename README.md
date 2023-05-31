aa6c2d8d11b33f2bcbc91be7201996470821dfc2
appeye <appeolsson@hotmail.com>
2023-05-31 03:06:05 +0200

    wrote README

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # jr-hint
+
+This is a small web component that allows you to display custom messages when hovering elements.
+It has very minimal styling, so up to you to style it to your liking.
+
+##Install
+
+```bash
+npm install jr-hint
+```
+
+## Import
+
+```js
+import 'jr-hint';
+```
+
+or
+
+```html
+<script src="node_modules/jr-hint" type="module"></script>
+```
+
+## Usage
+
+Use an id or class name as the attribute name, and the message as the value. Message will be attached to all elements with that class name.
+
+```html
+<div id="square"></div>
+<div class="circle"></div>
+<div class="circle"></div>
+
+<jr-hint
+	#square="A square message..."
+	.circle="A circular message..."
+></jr-hint>
+```
+
+## Styling example
+
+```css
+jr-hint {
+    color: magenta;
+}
---
+++ index.js
@@ -0,0 +1,70 @@
+const HintTemplate = document.createElement("template");
+HintTemplate.innerHTML = `
+	<style>
+		:host {
+			display: flex;
+			visibility: visible;
+			flex-direction: row;
+			justify-content: flex-start;
+			position: absolute;
+			bottom: 0;
+			width: 100%;
+			height: auto;
+			background-color: black;
+			color: gold;
+			user-select:none;
+			pointer-events: none;
+		}
+		
+		:host(.visible) {
+			visibility: visible;
+		}
+			
+	</style>
+	
+	<slot></slot>
+`;
+
+class JRHint extends HTMLElement {
+	constructor() {
+		super();
+		const shadow = this.attachShadow({mode: 'open'});
+		shadow.append(HintTemplate.content.cloneNode(true));
+
+		this.hintElement = shadow.querySelector('.hint');
+		this.innerHTML = "";
+		this.writeToConsole = this.getAttribute("console");
+
+		let hints = this.getAttributeNames();
+		let selection;
+
+		for(let i=0; i<hints.length; i++) {
+
+			if(!document.querySelector(hints[i])){
+				continue;
+			}
+
+			selection = document.querySelectorAll(hints[i]);
+
+			selection.forEach((element) => {
+				element.addEventListener('mouseover', () => {
+					if (this.writeToConsole !== null) {
+						console.log(this.getAttribute(hints[i]));
+					} else {
+						this.innerHTML = this.getAttribute(hints[i]);;
+						this.classList.add("visible");
+					}
+				});
+				element.addEventListener('mouseleave', () => {
+					this.innerHTML = ""
+					this.classList.remove("visible");
+				});
+			});
+
+		};
+
+	}
+
+}
+
+customElements.define("jr-hint", JRHint);
