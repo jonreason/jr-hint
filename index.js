94f0a5a8c36749af6c921cd57e5115a986365cd3
appeye <appeolsson@hotmail.com>
2023-06-08 10:35:12 +0200

    moved template code into class

--- index.js
+++ index.js
@@ -1,40 +1,38 @@
-const HintTemplate = document.createElement("template");
-HintTemplate.innerHTML = `
-	<style>
-		:host {
-			display: flex;
-			visibility: visible;
-			flex-direction: row;
-			justify-content: flex-start;
-			position: absolute;
-			bottom: 0;
-			width: 100%;
-			height: auto;
-			background-color: black;
-			color: gold;
-			user-select:none;
-			pointer-events: none;
-		}
-		
-		:host(.visible) {
-			visibility: visible;
-		}
-			
-	</style>
-	
-	<slot></slot>
-`;
-
 export default class JRHint extends HTMLElement {
 	constructor() {
 		super();
 
+		const HintTemplate = document.createElement("template");
+		HintTemplate.innerHTML = `
+		<style>
+			:host {
+				display: flex;
+				visibility: visible;
+				flex-direction: row;
+				justify-content: flex-start;
+				position: absolute;
+				bottom: 0;
+				width: 100%;
+				height: auto;
+				background-color: black;
+				color: gold;
+				user-select:none;
+				pointer-events: none;
+			}
+			
+			:host(.visible) {
+				visibility: visible;
+			}
+			
+		</style>
+	
+		<slot></slot>
+		`;
 
 
 		const shadow = this.attachShadow({mode: 'open'});
 		shadow.append(HintTemplate.content.cloneNode(true));
 
-		this.hintElement = shadow.querySelector('.hint');
 		this.innerHTML = "";
 		this.writeToConsole = this.getAttribute("console");
 
